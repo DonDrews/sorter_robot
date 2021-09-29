a45335acdca3a83b12e7043a17be45963e05000a
alex <bip760@icloud.com>
2021-09-29 13:46:08 -0700

    Updated crop code

--- crop.py
+++ crop.py
@@ -3,17 +3,16 @@ import cv2
 
 im = Image.open('card.png')
 
+width, height = im.size   # Get dimensions
+print(width, height)
+
 im_cropName = im.crop((60, 30, 950, 200))
 im_cropName.save('cropCardName.png', quality=95)
 
-width, height = im.size   # Get dimensions
-#print(width, height)
-
 #(left, top, right, bottom)
-#im_cropSet = im.crop((996, 1131, 942, 1046))
 im_cropSet = im.crop((1000, 945, 1128, 1044))
 im_cropSet.save('cropCardSet.png', quality=95)
 
-crop = cv2.imread('cropCardSet.png')
+crop = cv2.imread('cropCardName.png')
 cv2.imshow("Cropped Set", crop)
 cv2.waitKey(0)
