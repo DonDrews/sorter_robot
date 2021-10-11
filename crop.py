cb71ca575025daf634b384117823443326723e19
alex <bip760@icloud.com>
2021-10-11 13:50:34 -0700

    Updated crop and added in FFT and Compare code

--- crop.py
+++ crop.py
@@ -1,18 +1,18 @@
 from PIL import Image
 import cv2
 
-im = Image.open('card.png')
+im = Image.open('cmdr.png')
 
 width, height = im.size   # Get dimensions
 print(width, height)
 
-im_cropName = im.crop((60, 30, 950, 200))
-im_cropName.save('cropCardName.png', quality=95)
+#im_cropName = im.crop((286, 820, 1293, 1001))
+#im_cropName.save('cropCardName.png', quality=95)
 
 #(left, top, right, bottom)
-im_cropSet = im.crop((1000, 945, 1128, 1044))
+im_cropSet = im.crop((1777, 2178, 1953, 2327))
 im_cropSet.save('cropCardSet.png', quality=95)
 
-crop = cv2.imread('cropCardName.png')
+crop = cv2.imread('cropCardSet.png')
 cv2.imshow("Cropped Set", crop)
 cv2.waitKey(0)
