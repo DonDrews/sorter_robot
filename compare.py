79d9ce554f6a1b44be23df81e3017dc46740f786
alex <bip760@icloud.com>
2021-12-15 16:37:29 -0800

    Furthered set comparison with visuals

--- compare.py
+++ compare.py
@@ -2,30 +2,57 @@ from PIL import Image
 from PIL import ImageChops
 from math import sqrt
 import numpy as np
-import cv2
+import matplotlib.pyplot as plt
+import os, os.path
 
-def setUp(imageA, imageB, imageC):
+
+def importSets():
+	imgs = []
+	valid_images = [".jpg",".gif",".png",".tga"]
+	path = '/home/alex/Desktop/sorter_robot/set_symbols'
+	
+	for f in os.listdir(path):
+		ext = os.path.splitext(f)[1]
+		if ext.lower() not in valid_images:
+			continue
+
+		imgs.append(Image.open(os.path.join(path,f)))
+	
+	return imgs
+
+def setUp(imageA, fileName):
 	new = (400, 400) # This will have to be adjusted once we are on the real sorter
+	fileType = '.png'
 
 	# resize the images
 	imageA = imageA.resize(new)
-	imageB = imageB.resize(new)
-	imageC = imageC.resize(new)
 
+	# grey scale
 	imageA = imageA.convert('L')
-	imageB = imageB.convert('L')
-	imageC = imageC.convert('L')
 
-	imageA = imageA.point(lambda v: v >= 128 and 255, "1")
-	imageB = imageB.point(lambda v: v >= 128 and 255, "1")
-	imageC = imageC.point(lambda v: v >= 128 and 255, "1")
+	# threshold scale
+	imageA = imageA.point(lambda x: x >= 128 and 255, "1")
 
 	# save after resize
-	imageA.save("temp.png")
-	imageB.save("temp2.png")
-	imageC.save("temp3.png")
- 
+	imageA.save(str(fileName)+fileType)
 
+def setUpTest(imageA):
+	new = (400, 400) # This will have to be adjusted once we are on the real sorter
+	fileType = '.png'
+
+	# resize the images
+	imageA = imageA.resize(new)
+
+	# grey scale
+	imageA = imageA.convert('L')
+
+	# threshold scale
+	imageA = imageA.point(lambda x: x >= 128 and 255, "1")
+
+	# save after resize
+	imageA.save('cropCardSet.png')
+
+ 
 def calcDiff(imageA, imageB):
     # calculate the root-mean-square difference between two images
     dif = ImageChops.difference(imageA, imageB)
@@ -33,21 +60,32 @@ def calcDiff(imageA, imageB):
 
 def main():
 	im = Image.open("cropCardSet.png")
-	im2 = Image.open("mid-c.png")
-	im3 = Image.open("theros.png") # the test comparison set
-
-	setUp(im, im2, im3)
+	compList = list()
 	
-	im = Image.open("temp.png")
-	im2 = Image.open("temp2.png")
-	im3 = Image.open("temp3.png")
+	setList = importSets()
 
-	compSame = calcDiff(im, im2)
-	compDiff = calcDiff(im, im3)
+	for i, value in enumerate(setList):
+		setUp(value, i)
 
-	print("Same set symbol comp: ", compSame)
-	print("Different set symbol comp: ", compDiff)
-	
+	setUpTest(im)
+	im = Image.open("cropCardSet.png")
+
+	for i in range(len(setList)):
+		try:
+			tempIm = Image.open(str(i)+'.png')
+		except:
+			break
+
+		compList.append(calcDiff(im, tempIm))
+
+	print(compList)
+
+	plt.plot(compList)
+	plt.ylabel('Card Match (Lower is better)')
+	plt.xticks([0,1,2,3,4], ['Card 0', 'Card 1', 'Card 2', 'Card 3', 'Card 4'], rotation ='horizontal')
+	plt.xlabel('Card Number')
+	plt.show()
+
 if __name__=="__main__":
     main()
 
--- crop.py
+++ crop.py
@@ -1,22 +1,24 @@
 from PIL import Image
 import cv2
 
-im = Image.open('MID.png')
+im = Image.open('poopy.png')
 
 width, height = im.size   # Get dimensions
 print(width, height)
 
-#im_cropName = im.crop((286, 820, 1293, 1001))
-#im_cropName.save('cropCardName.png', quality=95)
+im_cropName = im.crop((717, 165, 990, 291))
+im_cropName.save('cropCardName.png', quality=95)
 
 #(left, top, right, bottom)
-im_crop = im.crop((1810, 2145, 1997, 2316))
+im_crop = im.crop((1818, 1152, 1920, 1269))
 im_crop.save('cropCardSet.png', quality=95)
 
 im2 = Image.open('cropCardSet.png')
-im2_crop = im2.crop((34, 17, 173, 145))
+im2_crop = im2.crop((5, 25, 84, 91))
 im2_crop.save('cropCardSet.png', quality=95)
 
 crop = cv2.imread('cropCardSet.png')
 cv2.imshow("Cropped Set", crop)
 cv2.waitKey(0)
+
+1920, 1269
