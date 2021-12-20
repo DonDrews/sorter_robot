a13c2e6d2f2e45f352091fa3b10eb378ea0a79ae
alex <bip760@icloud.com>
2021-12-20 15:06:44 -0800

    Optimized compare script and improved graph readability

--- compare.py
+++ compare.py
@@ -1,98 +1,75 @@
-from PIL import Image
-from PIL import ImageChops
+from PIL import Image, ImageChops
 from math import sqrt
 import numpy as np
 import matplotlib.pyplot as plt
 import os, os.path
 
-
 def importSets():
-	imgs = []
-	valid_images = [".jpg",".gif",".png",".tga"]
-	path = '/home/alex/Desktop/sorter_robot/set_symbols'
+	imgs = dict()
+	valid_format = [".png"]
+	path = '/home/alex/Desktop/sorter_robot/set_symbols' # Swap once on the PI
 	
 	for f in os.listdir(path):
 		ext = os.path.splitext(f)[1]
-		if ext.lower() not in valid_images:
+		if ext.lower() not in valid_format:
 			continue
 
-		imgs.append(Image.open(os.path.join(path,f)))
+		imgs[f] = Image.open(os.path.join(path,f))
 	
 	return imgs
 
-def setUp(imageA, fileName):
-	new = (400, 400) # This will have to be adjusted once we are on the real sorter
-	fileType = '.png'
+def setUp(imageName, image): # Set up for the comparison image
+	new = (400, 400) # This will have to be adjusted once the PI
 
-	# resize the images
-	imageA = imageA.resize(new)
+	# Resize the images
+	image = image.resize(new)
 
-	# grey scale
-	imageA = imageA.convert('L')
+	# Grey scale
+	image = image.convert('L')
 
-	# threshold scale
-	imageA = imageA.point(lambda x: x >= 128 and 255, "1")
+	# Threshold scale
+	image = image.point(lambda x: x >= 128 and 255, "1")
 
-	# save after resize
-	imageA.save(str(fileName)+fileType)
-
-def setUpTest(imageA):
-	new = (400, 400) # This will have to be adjusted once we are on the real sorter
-	fileType = '.png'
-
-	# resize the images
-	imageA = imageA.resize(new)
-
-	# grey scale
-	imageA = imageA.convert('L')
-
-	# threshold scale
-	imageA = imageA.point(lambda x: x >= 128 and 255, "1")
-
-	# save after resize
-	imageA.save('cropCardSet.png')
-
+	# Save after resize
+	image.save(imageName)
  
 def calcDiff(imageA, imageB):
-    # calculate the root-mean-square difference between two images
+    # Calculate the root-mean-square difference between two images
     dif = ImageChops.difference(imageA, imageB)
     return [sqrt(np.mean(np.square(np.array(dif)))), np.mean(np.array(dif))]
 
 def main():
-	im = Image.open("cropCardSet.png")
 	compList = list()
-	
+	graphAxis = list()
+	counterList = list()
+	counter = -1 # Zero based indexing
+	sep = '.'
+	im = Image.open("cropCardSet.png")
+
+	setUp("cropCardSet.png", im)
+	im = Image.open("cropCardSet.png")
+
 	setList = importSets()
 
-	for i, value in enumerate(setList):
-		setUp(value, i)
+	for i in setList:
+		setUp(i, setList[i]) # i is card name, setList[i] is the image
 
-	setUpTest(im)
-	im = Image.open("cropCardSet.png")
-
-	for i in range(len(setList)):
+	for i in setList:
 		try:
-			tempIm = Image.open(str(i)+'.png')
+			tempIm = Image.open(i)
 		except:
 			break
 
+		counter += 1
+		counterList.append(counter)
+		graphAxis.append(str(i).split(sep, 1)[0])
 		compList.append(calcDiff(im, tempIm))
-
-	print(compList)
 
 	plt.plot(compList)
 	plt.ylabel('Card Match (Lower is better)')
-	plt.xticks([0,1,2,3,4], ['Card 0', 'Card 1', 'Card 2', 'Card 3', 'Card 4'], rotation ='horizontal')
-	plt.xlabel('Card Number')
+	plt.xticks(counterList, graphAxis, rotation ='horizontal')
+	plt.xlabel('Set')
 	plt.show()
 
 if __name__=="__main__":
     main()
-
-
-
-
-
-
-
-
